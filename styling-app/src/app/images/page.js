0f472b8bf694e4608642b8494121da9f348e457f
unknown <sasubramanian_md@hotmail.com>
2026-07-10 10:05:52 +0530

    components

---
+++ styling-app/src/app/images/page.js
@@ -0,0 +1,9 @@
+import LocalImage from "@/app/images/component/LocalImage";
+import RemoteImage from "@/app/images/component/RemoteImage";
+
+export default function ImagePage(){
+    return <div>
+        <LocalImage/>
+        <RemoteImage/>
+    </div>
+}
